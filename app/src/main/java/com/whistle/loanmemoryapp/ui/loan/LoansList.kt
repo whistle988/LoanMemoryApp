846e3bcb82c1f8c87b17c34231c65f71d946be3c
ElenaVasileva <chageshii@gmail.com>
2024-01-03 02:28:48 +0900

    Fixed MAinScreen layout, added checkbox and @PreviewParameter

--- app/src/main/java/com/whistle/loanmemoryapp/ui/loan/LoansList.kt
+++ app/src/main/java/com/whistle/loanmemoryapp/ui/loan/LoansList.kt
@@ -18,16 +18,22 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.whistle.loanmemoryapp.R
 import com.whistle.loanmemoryapp.data.model.Loan
 import com.whistle.loanmemoryapp.ui.theme.LoanMemoryAppTheme
@@ -53,6 +59,7 @@ fun LoanCard(
     name: String?,
     amountMonth: String?,
     navController: NavController) {
+    val checkedState = remember { mutableStateOf(false) }
     Card(
         modifier = Modifier
             .padding(10.dp)
@@ -63,9 +70,11 @@ fun LoanCard(
             },
         shape = MaterialTheme.shapes.medium,
         elevation = CardDefaults.cardElevation( defaultElevation = 5.dp ),
-        colors = CardDefaults.cardColors( containerColor = MaterialTheme.colorScheme.surfaceVariant )
+        colors = CardDefaults.cardColors( containerColor = MaterialTheme.colorScheme.surfaceVariant ),
     ) {
-        Row(Modifier.padding(all = 8.dp)) {
+        Row(
+            modifier = Modifier.padding(8.dp).fillMaxWidth()
+        ) {
             Image(
                 painter = painterResource(id = R.drawable.ic_launcher_foreground),
                 contentDescription = null,
@@ -73,11 +82,15 @@ fun LoanCard(
                     .size(40.dp)
                     .clip(CircleShape)
                     .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+                    .padding(8.dp)
+                    .align(Alignment.CenterVertically)
             )
 
             Spacer(modifier = Modifier.width(8.dp))
 
-            Column {
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
                 Text(
                     text = name!!,
                     color = MaterialTheme.colorScheme.secondary,
@@ -94,6 +107,29 @@ fun LoanCard(
                     )
                 }
             }
+            Column(
+                modifier = Modifier.align(Alignment.CenterVertically)
+            ) {
+                Checkbox(
+                    checked = checkedState.value,
+                    onCheckedChange = {checkedState.value = it}
+                )
+            }
         }
     }
+}
+
+@Preview("ListPreview")
+@Composable
+fun LoanListPreview() {
+    val sampleLoanList = remember { mutableStateOf(
+        listOf(
+            Loan(1, "VTB", "01.01.2022", "2000"),
+            Loan(2, "Sample Loan 2", "02.02.2022", "3000"),
+            Loan(3, "Sample Loan 2", "02.02.2022", "3000"),
+            Loan(4, "Sample Loan 2", "02.02.2022", "3000"),
+            Loan(5, "Sample Loan 2", "02.02.2022", "3000")
+        )
+    )}
+    LoanList(loanList = sampleLoanList, navController = rememberNavController())
 }
